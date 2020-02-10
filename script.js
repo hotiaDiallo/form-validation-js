0c7189224c914cfac9809442a277888bd372132b
ibrahima <ibrahimahotia.diallo@gmail.com>
2020-02-10 21:51:32 +0100

    email with regex and confirm password

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 
+
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+      showSuccess(input);
+    } else {
+      showError(input, 'Email is not valid');
+    }
+}
+
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+      showError(input2, 'Passwords do not match');
+    }
+}
+
 function checkRequired(input){
     input.forEach(input => {
         if(input.value.trim() === ""){
@@ -44,5 +60,7 @@ form.addEventListener('submit', function(e) {
     checkRequired([username, email, password, password2]);
     checkInputLength(username, 5, 10);
     checkInputLength(password, 8, 20);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
 });
   
